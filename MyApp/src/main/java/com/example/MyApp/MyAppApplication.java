8c065ccf4afde9fb11e8af0a36b0484585f7fd7e
xanvalza <xanvalza@gmail.com>
2023-05-16 21:45:50 -0500

    add msg

--- MyApp/src/main/java/com/example/MyApp/MyAppApplication.java
+++ MyApp/src/main/java/com/example/MyApp/MyAppApplication.java
@@ -2,12 +2,15 @@ package com.example.MyApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MyAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyAppApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(MyAppApplication.class, args);
+		System.out.println(context.getApplicationName());
 	}
 
 }
